63b1538ff804552694bf0a2149a69e8e84f9cc73
Geut Galai <59103837+Geutg@users.noreply.github.com>
2023-11-09 18:42:47 +0200

    rearranging code to make more sense
    there was code that uses the coocurence network on script 3 even though the networks are constructed only after the exploratory analysis, and the discription of the script didnt make sense.
    this code was moved to cooccurrence_network_analysis.r as it does what this script is meant to do.

--- 3.exploratory_analysis.r
+++ 3.exploratory_analysis.r
@@ -136,41 +136,16 @@ cows_per_farm %>%
   theme(axis.text = element_text(size=10, color='black'), 
         title = element_text(size=14), axis.text.x = element_text(angle = 90))
 
-## Number of co-occurrence links per farm -----
-farm_multilayer_pos <- read_csv('local_output/farm_multilayer_pos_30.csv')
-links_per_farm <- farm_multilayer_pos %>%
-  group_by(level_name) %>%
-  mutate(link=1) %>%
-  summarise(links=sum(link))
 
-links_per_farm %>%
-  ggplot(aes(level_name, links)) +
-  geom_col(fill='dark green', color='white') +
-  theme_bw() +
-  labs(y = 'Number of links') +
-  theme(axis.text = element_text(size=10, color='black'), title = element_text(size=14), axis.text.x = element_text(angle = 90))
-
-## Density (connectance per farm) -------
-farm_net_density <- function(d){
-  g <- graph_from_data_frame(d, directed = FALSE, vertices = NULL)
-  out <- tibble(d=round(igraph::edge_density(g),2))
-  return(out)
-}
-
-farm_density <- 
-  farm_multilayer_pos %>% 
-  group_by(level_name) %>% 
-  group_modify(~farm_net_density(.x))
-  
-# Summary table for farms --------
+# Save ASV Summary table for the farms ----
+# This it part of Table 1 in the paper
+# the rest will be completed after constructing the networks on the HPC
+# on scripts "cooccurrence_network_analysis.r"
 cows_per_farm %>%
   left_join(richness_per_farm) %>%
   left_join(Richness_per_cow_farm %>% select(Farm, ASV_summary)) %>% 
-  left_join(links_per_farm, by = c('Farm'='level_name')) %>%
-  left_join(farm_density, by = c('Farm'='level_name')) %>%
-  as_tibble() %>% 
-  rename(Cows=cow_num, ASVs=ASV_Richness, `ASVs per cow`=ASV_summary,Links=links,Density=d) %>% 
-  write_csv('local_output/paper_table1.csv')
+  write_csv('local_output/summary_table_pre_network.csv')
+
 
 # Cows summary across farms ------
 ASV_Core_30 %>%
@@ -299,116 +274,4 @@ ASV_Core_30 %>%
   labs(x='Number of farms', y='Count')+
   scale_x_continuous(breaks = seq(0,7,1))+
   paper_figs_theme_no_legend
-
-
-# Clustering coefficient---------------------
-farm_multilayer_pos <- read_csv('local_output/farm_multilayer_pos_30.csv')
-
-CC_obs <- 
-  farm_multilayer_pos %>%
-  group_by(level_name) %>% 
-  group_modify(~calc_CC_local(.x)) %>%
-  drop_na()
-
-
-# Distributions of observed CC
-pdf('/Users/Geut/Dropbox/for_processing/rumen/cc_farm_boxplot.pdf', 5, 5)
-CC_obs %>% 
-  filter(k>=10) %>% 
-  mutate(level_name=factor(level_name, levels = c("UK1","UK2","IT1","IT2","IT3","FI1",'SE1'))) %>%
-  ggplot(aes(x=level_name, y=CC))+
-  geom_boxplot()+
-  theme_bw() +
-  labs(y='Clustering coefficient', x='Farm') +
-  paper_figs_theme_no_legend
-dev.off()
-
-# CC statistics in each farm
-CC_obs %>%
-  filter(k>=10) %>% 
-  summarise(cc_mean=mean(CC),
-            cc_median=median(CC),
-            cc_sd=sd(CC),
-            cc_min=min(CC),
-            cc_max=max(CC)) %>% 
-  mutate(ASV_summary=paste(cc_mean,' [',cc_min,'-',cc_max,']',sep=''))
-
-# Partner Fidelity with Jaccard ---------------------------
-# Because this is an undirected network, not all ASVs are in the from column,
-# which we use for the analysis. So duplicate the links to ensure that all ASVs
-# are in the from column.
-setdiff(farm_multilayer_pos$from,farm_multilayer_pos$to) # These ASVs were missing
-setdiff(farm_multilayer_pos$to,farm_multilayer_pos$from) # These ASVs were missing
-
-farm_multilayer_pos_final <- 
-  bind_rows(farm_multilayer_pos, 
-            farm_multilayer_pos %>%
-              relocate(to, from) %>%
-              rename(to=from, from=to))
-
-n_distinct(farm_multilayer_pos$from)
-n_distinct(farm_multilayer_pos_final$from)
-setdiff(farm_multilayer_pos_final$from,farm_multilayer_pos$to) # These ASVs were missing
-
-# keep only ASVs that occur in 2 or more farms
-farm_multilayer_pos_final %<>%
-  group_by(from) %>%
-  mutate(num_farms_from=n_distinct(level_name)) %>%
-  filter(num_farms_from>=2)
-
-## PF_J observed network ---------------------------
-PF_J_obs <-
-  farm_multilayer_pos_final %>%
-  group_by(from) %>%
-  group_modify(~calculate_PF_J(.x))
-
-PF_J_obs <- as_tibble(PF_J_obs)
-
-# Partner Fidelity with UniFrac ---------------------------
-
-## PF_U observed network ---------------------------
-
-# read tree from phylo data
-# set working directory
-phylo_tree <- readRDS("local_output/fitted_asvs_phylo_tree.rds")
-tree <- phylo_tree$tree
-# tree <- readRDS("local_output/rooted_phylo_tree.rds") # only for 5%
-
-PF_U_obs <-
-  farm_multilayer_pos_final %>%
-  group_by(from) %>%
-  group_modify(~calculate_PF_U(.x, tree))
-names(PF_U_obs) <- c("from", "PF_U", "PF_U_sd", "num_layers","UniFrac_type")
-PF_U_obs %<>% filter(UniFrac_type=='d_UW')
-PF_U_obs$PF_U=1-PF_U_obs$PF_U # Work with similarity instead of dissimilarity
-PF_U_obs <- as_tibble(PF_U_obs)
-
-# Plot Jaccard and UniFrac for paper ------------------------------------------
-PF_J_obs <- read_csv('local_output/PF_J_pos_30_obs.csv') # change the number according to the filter level
-PF_U_obs <- read_csv('local_output/PF_U_pos_30_obs.csv')
-
-pdf('/Users/Geut/Dropbox/for_processing/rumen/cc_pf_hist.pdf', 5, 6)
-PF_score_plot <- 
-  bind_rows(
-    PF_J_obs %>% select(from, PF=PF_J) %>% mutate(type='Jaccard'),
-    PF_U_obs %>% select(from, PF=PF_U) %>% mutate(type='UniFrac')
-  ) %>% 
-  ggplot(aes(PF, fill=type)) +
-  geom_histogram(alpha=1, color='white')+
-  labs(x='Partner fidelity score', y='Count')+
-  paper_figs_theme
-PF_score_plot
-dev.off()
-
-bind_rows(
-  PF_U_obs %>% select(from, PF=PF_U) %>% mutate(type='UniFrac')
-) %>% 
-  ggplot(aes(PF)) +
-  geom_histogram(alpha=1, color='white', fill='lightblue')+
-  labs(x='UniFrac score', y='Count')+
-  # facet_grid(~type)+
-  # geom_vline(xintercept = c(-1.96, 1.96), color = 'red')+
-  # geom_vline(xintercept = c(-2.5, 2.5), color = 'red', linetype='dashed')+
-  paper_figs_theme
-
 
--- 5.cooccurrence_network_analysis.r
+++ 5.cooccurrence_network_analysis.r
@@ -47,3 +47,154 @@ setwd('../../')
 
 # save the positive edges
 write_csv(farm_multilayer, 'local_output/farm_multilayer_pos_30.csv')
+
+
+# Analyse the structure ------------------------- 
+
+# General network attributes -----------------
+## Number of co-occurrence links per farm -----
+farm_multilayer_pos <- read_csv('local_output/farm_multilayer_pos_30.csv')
+links_per_farm <- farm_multilayer_pos %>%
+  group_by(level_name) %>%
+  mutate(link=1) %>%
+  summarise(links=sum(link))
+
+links_per_farm %>%
+  ggplot(aes(level_name, links)) +
+  geom_col(fill='dark green', color='white') +
+  theme_bw() +
+  labs(y = 'Number of links') +
+  theme(axis.text = element_text(size=10, color='black'), title = element_text(size=14), axis.text.x = element_text(angle = 90))
+
+## Density (connectance per farm) -------
+farm_net_density <- function(d){
+  g <- graph_from_data_frame(d, directed = FALSE, vertices = NULL)
+  out <- tibble(d=round(igraph::edge_density(g),2))
+  return(out)
+}
+
+farm_density <- 
+  farm_multilayer_pos %>% 
+  group_by(level_name) %>% 
+  group_modify(~farm_net_density(.x))
+
+# complete the table started on exploratory_analysis.r to make paper table 1
+incomplete_table <- read_csv('local_output/summary_table_pre_network.csv')
+incomplete_table %>% 
+  left_join(links_per_farm, by = c('Farm'='level_name')) %>%
+  left_join(farm_density, by = c('Farm'='level_name')) %>%
+  as_tibble() %>% 
+  rename(Cows=cow_num, ASVs=ASV_Richness, `ASVs per cow`=ASV_summary,Links=links,Density=d) %>% 
+  write_csv('local_output/paper_table1.csv')
+
+
+# Clustering coefficient---------------------
+farm_multilayer_pos <- read_csv('local_output/farm_multilayer_pos_30.csv')
+
+CC_obs <- 
+  farm_multilayer_pos %>%
+  group_by(level_name) %>% 
+  group_modify(~calc_CC_local(.x)) %>%
+  drop_na()
+
+
+# Distributions of observed CC
+pdf('/Users/Geut/Dropbox/for_processing/rumen/cc_farm_boxplot.pdf', 5, 5)
+CC_obs %>% 
+  filter(k>=10) %>% 
+  mutate(level_name=factor(level_name, levels = c("UK1","UK2","IT1","IT2","IT3","FI1",'SE1'))) %>%
+  ggplot(aes(x=level_name, y=CC))+
+  geom_boxplot()+
+  theme_bw() +
+  labs(y='Clustering coefficient', x='Farm') +
+  paper_figs_theme_no_legend
+dev.off()
+
+# CC statistics in each farm
+CC_obs %>%
+  filter(k>=10) %>% 
+  summarise(cc_mean=mean(CC),
+            cc_median=median(CC),
+            cc_sd=sd(CC),
+            cc_min=min(CC),
+            cc_max=max(CC)) %>% 
+  mutate(ASV_summary=paste(cc_mean,' [',cc_min,'-',cc_max,']',sep=''))
+
+# Partner Fidelity with Jaccard ---------------------------
+# Because this is an undirected network, not all ASVs are in the from column,
+# which we use for the analysis. So duplicate the links to ensure that all ASVs
+# are in the from column.
+setdiff(farm_multilayer_pos$from,farm_multilayer_pos$to) # These ASVs were missing
+setdiff(farm_multilayer_pos$to,farm_multilayer_pos$from) # These ASVs were missing
+
+farm_multilayer_pos_final <- 
+  bind_rows(farm_multilayer_pos, 
+            farm_multilayer_pos %>%
+              relocate(to, from) %>%
+              rename(to=from, from=to))
+
+n_distinct(farm_multilayer_pos$from)
+n_distinct(farm_multilayer_pos_final$from)
+setdiff(farm_multilayer_pos_final$from,farm_multilayer_pos$to) # These ASVs were missing
+
+# keep only ASVs that occur in 2 or more farms
+farm_multilayer_pos_final %<>%
+  group_by(from) %>%
+  mutate(num_farms_from=n_distinct(level_name)) %>%
+  filter(num_farms_from>=2)
+
+## PF_J observed network:
+PF_J_obs <-
+  farm_multilayer_pos_final %>%
+  group_by(from) %>%
+  group_modify(~calculate_PF_J(.x))
+
+PF_J_obs <- as_tibble(PF_J_obs)
+
+# Partner Fidelity with UniFrac ---------------------------
+
+# read tree from phylo data
+# set working directory
+phylo_tree <- readRDS("local_output/fitted_asvs_phylo_tree.rds")
+tree <- phylo_tree$tree
+
+## PF_U observed network:
+PF_U_obs <-
+  farm_multilayer_pos_final %>%
+  group_by(from) %>%
+  group_modify(~calculate_PF_U(.x, tree))
+names(PF_U_obs) <- c("from", "PF_U", "PF_U_sd", "num_layers","UniFrac_type")
+PF_U_obs %<>% filter(UniFrac_type=='d_UW')
+PF_U_obs$PF_U=1-PF_U_obs$PF_U # Work with similarity instead of dissimilarity
+PF_U_obs <- as_tibble(PF_U_obs)
+
+# Plot Jaccard and UniFrac for paper ------------------------------------------
+PF_J_obs <- read_csv('local_output/PF_J_pos_30_obs.csv') # change the number according to the filter level
+PF_U_obs <- read_csv('local_output/PF_U_pos_30_obs.csv')
+
+pdf('/Users/Geut/Dropbox/for_processing/rumen/cc_pf_hist.pdf', 5, 6)
+PF_score_plot <- 
+  bind_rows(
+    PF_J_obs %>% select(from, PF=PF_J) %>% mutate(type='Jaccard'),
+    PF_U_obs %>% select(from, PF=PF_U) %>% mutate(type='UniFrac')
+  ) %>% 
+  ggplot(aes(PF, fill=type)) +
+  geom_histogram(alpha=1, color='white')+
+  labs(x='Partner fidelity score', y='Count')+
+  paper_figs_theme
+PF_score_plot
+dev.off()
+
+bind_rows(
+  PF_U_obs %>% select(from, PF=PF_U) %>% mutate(type='UniFrac')
+) %>% 
+  ggplot(aes(PF)) +
+  geom_histogram(alpha=1, color='white', fill='lightblue')+
+  labs(x='UniFrac score', y='Count')+
+  # facet_grid(~type)+
+  # geom_vline(xintercept = c(-1.96, 1.96), color = 'red')+
+  # geom_vline(xintercept = c(-2.5, 2.5), color = 'red', linetype='dashed')+
+  paper_figs_theme
+
+
+
