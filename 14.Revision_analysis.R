af4e20ed0806d41a28d193f85064d3d341ece3ed
Geut Galai <59103837+Geutg@users.noreply.github.com>
2023-12-16 23:13:17 +0200

    start revision work

---
+++ 14.Revision_analysis.R
@@ -0,0 +1,26 @@
+# ------ 14.Revision_analysis.r ----------------------------------------
+# This script temporarily holds the analysis requested in the revision
+# they will later be moved to more logically relevant places in the repo
+#-----------------------------------------------------------------------
+
+#------ includes ----------
+library(tidyverse)
+library(magrittr)
+library(reshape2)
+
+#------ run ------------
+# Transitivity ---------
+# here will be the probability based transitivity
+
+
+# phylogenetic partner fidelity ----------
+
+
+
+# modularity - phylogenetic composition --------
+# includes randomality check
+
+
+
+# NMI of clusters and hypothesis ---------
+
